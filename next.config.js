031728b344dc7cfde1d2fc0ac07adfc032fdb965
q <q@q.com>
2020-10-24 14:02:20 +0330

    remove graphql expose

--- next.config.js
+++ next.config.js
@@ -1,12 +1,1 @@
-const { WP_BASE_URL } = process.env;
-
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/graphql",
-        destination: `${WP_BASE_URL}/graphql`,
-      },
-    ];
-  },
-};
+module.exports = {};
