f7d83b578941b0e781ecfa4831225dc8ca60cd6d
Sajjad Hashemian <wolaws@gmail.com>
2021-01-09 19:57:15 +0330

    cleanup articles

--- perPageComponenta/Articles/View.tsx
+++ perPageComponenta/Articles/View.tsx
@@ -1,66 +1,19 @@
 import SEO from "../../components/seo";
 
 import BlogList from "./blogList";
-import styled from "styled-components";
 import { Posts_posts_nodes } from "../../wpapi";
-import Hero from "../../components/Hero";
-import { device } from "../../consts/theme";
+import Menu from "../../components/menu";
 
 interface Props {
-  firstPost?: Posts_posts_nodes | null;
-  restPosts: (Posts_posts_nodes | null)[];
+  posts: (Posts_posts_nodes | null)[];
 }
-export const ArticlesView: React.FC<Props> = (props) => {
+
+export const ArticlesView = ({ posts }: Props) => {
   return (
     <div className="page">
       <SEO />
-      <Hero
-        upTitle={<UpTitle className="title">{`Start Learning Skills`}</UpTitle>}
-        downTitle={
-          <Title className="title">
-            {` to Make `}
-            <HighlightDesc>{`$$$`}</HighlightDesc>
-          </Title>
-        }
-        description={`Itachi Uchiha  is a fictional character in the Naruto manga and anime series created by Masashi Kishimoto. Itachi is the older brother of Sasuke Uchiha and is responsible for killing all the members of their clan, sparing only his younger brother Sasuke.`}
-        pic={<Pic src="/Lamp.png" alt="Featured image" />}
-      />
-      <BlogList {...props} />
+      <Menu />
+      <BlogList posts={posts} />
     </div>
   );
 };
-const Title = styled.h1`
-  line-height: 48px;
-  margin: 0 0 5px;
-  font-weight: 900;
-  margin-left: 5rem;
-  padding-right: 12rem;
-  font-size: 3.5vw;
-  line-height: 100%;
-  margin-bottom: 0;
-  @media ${device.laptop} {
-    font-size: 3vw;
-    padding-right: 6rem;
-  }
-  @media ${device.tablet} {
-    font-size: xx-large;
-    padding-right: 0rem;
-  }
-`;
-const UpTitle = styled(Title)`
-  margin-top: 3rem;
-`;
-const HighlightDesc = styled.span`
-  color: var(--primary-color-dark);
-`;
-const Pic = styled.img`
-  position: absolute;
-  top: 1rem;
-  width: 8em;
-  left: 2em;
-
-  @media ${device.laptop} {
-    width: 9rem;
-    top: 2em;
-  }
-`;
--- perPageComponenta/Articles/blogList.tsx
+++ perPageComponenta/Articles/blogList.tsx
@@ -1,38 +1,27 @@
 import PostCard from "../../components/post-card";
 import styled from "styled-components";
-import FirstArticle from "../../components/first-Article";
 import { device } from "../../consts/theme";
 import { Posts_posts_nodes } from "../../wpapi";
 
 interface Props {
-  firstPost?: Posts_posts_nodes | null;
-  restPosts: (Posts_posts_nodes | null)[];
+  posts: (Posts_posts_nodes | null)[];
 }
 
-const BlogList: React.FC<Props> = ({ firstPost, restPosts }) => {
+const BlogList = ({ posts }: Props) => {
   return (
-    <Section className={`home-posts `}>
-      <H2>Featured Article</H2>
-      {firstPost ? <FirstArticle {...firstPost} /> : null}
+    <Section className={`home-posts`}>
       <ArticlesContainer>
-        {restPosts.map((post) =>
+        {posts.map((post) =>
           post ? <PostCard key={post.id} {...post} /> : null,
         )}
       </ArticlesContainer>
-      {/* <Link href="/blog">
-        <div className="button">
-          See more
-          <span className="icon -right">
-            <RiArrowRightSLine />
-          </span>
-        </div>
-      </Link> */}
     </Section>
   );
 };
 export default BlogList;
 
 const Section = styled.section`
+  margin-top: 80px;
   padding-bottom: 100px;
   display: flex;
   flex-direction: column;
