2d5a3c75072b3cfd67df5c01b9ebafaec413c287
navidgoalpure <navid.goalpure@gmail.com>
2021-01-24 11:39:44 +0330

    parisa change 1 done implement nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,15 @@ import { AppProps } from "next/dist/next-server/lib/router/router";
 import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 const queryClient = new QueryClient();
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
