698cbed39d2cefb5f584e90cc6d23f7415677c88
navidgoalpure <navid.goalpure@gmail.com>
2021-03-18 00:52:32 +0330

    fix reset pass bug

--- pages/api/users/reset-password.ts
+++ pages/api/users/reset-password.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
-import { getTokenCookie } from "utils/auth-cookie";
 
 export default async function ResetPassword(
   req: NextApiRequest,
@@ -12,17 +11,9 @@ export default async function ResetPassword(
   }
 
   try {
-    const token = getTokenCookie(req);
-
     await axios.post(
       `https://wp.thehustleclub.com/wp-json/pl/v1/reset_password`,
       req.body,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      },
     );
     res.status(200).json({ success: true });
     return;
