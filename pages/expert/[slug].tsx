74ddfaa8d0427d5d8c01d452cb4c0c5001a2e5ad
navidgoalpure <navid.goalpure@gmail.com>
2021-04-16 17:09:47 +0430

    improve expert page

--- pages/expert/[slug].tsx
+++ pages/expert/[slug].tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import { EXPERT_LIST } from "consts/experts";
 import { Paper } from "elements/Layout";
 
-import { Body1, Body2, Caption } from "elements/typo";
+import { Body1, Body2, Body3, Caption } from "elements/typo";
 import styled from "styled-components";
 import Avatar from "components/Avatar";
 import LinkdeinIcon from "elements/SVGs/LinkdinIcon";
@@ -52,12 +52,30 @@ export default function ExpertUi() {
           <MeetingButton label="Set a meeting" to={`/expert/${slug}`} />
         </ReserveBlock>
       </TopPaper>
+      <MiddlePaper>
+        <Body1
+          style={{ marginBottom: "1rem", color: "var(--text-color-dark)" }}
+        >
+          How can I help you?
+        </Body1>
+        <Body3>
+          I'm the CEO of Dropbox and have faced enormous amounts of challenges
+          in the past years. I can help you about how to run, scale and grow
+          your company.I like to help entrepreneurs think through their business
+          strategy - how to scale a company, hire the right people to take it to
+          the next level or what kind of investors to get on board.
+        </Body3>
+      </MiddlePaper>
     </PageLayout>
   );
 }
 const TopPaper = styled(Paper)`
   display: flex;
   align-items: center;
+  margin-bottom: 30px;
+  max-width: 940px;
+  margin-right: auto;
+  margin-left: auto;
 `;
 const DataBlock = styled.div`
   display: flex;
@@ -85,4 +103,11 @@ const Price = styled(Body2)`
 `;
 const MeetingButton = styled(ButtonLink)`
   width: 100% !important;
+`;
+
+const MiddlePaper = styled(Paper)`
+  width: 100%;
+  max-width: 940px;
+  margin-right: auto;
+  margin-left: auto;
 `;
